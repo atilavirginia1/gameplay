cb96c0f1c8ff3eda91950545bcbe8fda70c383d8
atilavirginia1 <atilavirginia1@gmail.com>
2021-09-16 17:09:07 -0300

    SignIn Screen - React Native

---
+++ src/screens/SignIn/style.ts
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: theme.colors.secondary90
+    },
+
+    image:{
+        height: 360,
+        width: '100%',
+    },
+
+    title:{
+        color: theme.colors.heading,
+        textAlign: 'center',
+        fontSize: 30,
+        marginBottom: 16,
+        fontFamily: theme.fonts.title700,
+        lineHeight: 30
+    },
+
+    subtitle:{
+        color: theme.colors.heading,
+        textAlign: 'center',
+        fontSize: 14,
+        marginBottom: 44,
+        fontFamily: theme.fonts.title500,
+        lineHeight: 15
+    },
+
+    content:{
+        marginTop: -40,
+        paddingHorizontal: 50
+    }
+
+
+
+})
